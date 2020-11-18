9a67302f654809d58c0b2f5623e038c94f404c63
John <John@DESKTOP-IKTHH71>
2020-11-17 22:37:27 -0300

    Padroes Projetos 171120

---
+++ Builder/src/MethodChainingDemo.java
@@ -0,0 +1,14 @@
+// Driver class 
+public class MethodChainingDemo { 
+    public static void main(String args[]) 
+    { 
+        Student student1 = new Student(); 
+        Student student2 = new Student(); 
+  
+        student1.setId(1).setName("Ram").setAddress("Noida"); 
+        student2.setId(2).setName("Shyam").setAddress("Delhi"); 
+  
+        System.out.println(student1); 
+        System.out.println(student2); 
+    } 
+}
